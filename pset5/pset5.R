c00b531799c889dcf64403681905b3e0673be57e
afranzese99 <afranzese@uchicago.edu>
2021-05-20 20:44:46 -0500

    Completed Q5

--- pset5/pset5.R
+++ pset5/pset5.R
@@ -67,8 +67,8 @@ t.test(fem_wage, male_wage, conf.level = 0.95)
 fertility <- read_excel("fertility.xlsx")
 
 # (a) ==========================================================================
-weeksm_lm <- lm(fertility$weeksm1 ~ 1 + fertility$morekids)
-summary(weeksm_lm)
+weeksm_lm_mk <- lm(fertility$weeksm1 ~ 1 + fertility$morekids)
+summary(weeksm_lm_mk)
 
 ww_more_kids <- subset(fertility$weeksm1, fertility$morekids == 1)
 
@@ -95,6 +95,9 @@ t.test(ww_more_kids, ww_less_kids)
 ss_more_kids <- subset(fertility$samesex == 1, fertility$morekids == 1)
 ss_less_kids <- subset(fertility$samesex == 1, fertility$morekids == 0)
 
+weeksm_lm_ss <- lm(fertility$weeksm1 ~ 1 + fertility$samesex)
+summary(weeksm_lm_ss)
+
 t.test(ss_more_kids,ss_less_kids)
 
 
@@ -102,22 +105,36 @@ t.test(ss_more_kids,ss_less_kids)
 # third child, than couples whose first two children are not the same sex. 
 # The probability that a couple with its first two children being same sex has a 
 # third child is 54.99%. While the probability that a couple with its first two 
-# children being same sex not having has a third child is 47.83%. There is a 
-# 7.16% difference between these two likelihoods, this effect is relatively large.
+# children being same sex not having has a third child is 47.83%. 
+# The effect is large: 0.01431 - 9.502E-05 = 0.01421.
+
 # This is statistically significant given the p-value ( < 2.2e-16 ) is less than 
 # 0.05.
 
 # (d) ==========================================================================
 # Samesex may be a valid instrument for the IV regression of weeksworked on 
-# morekids because samesex has an effect on morekids, however, it does not have a
-# direct effect on weeksworked. Therefore cov(samesex, morekids) =/= 0
+# morekids because samesex has an effect on morekids, however, it does not have 
+# a direct effect on weeksworked. Therefore cov(samesex, morekids) =/= 0
 
 # (e) ==========================================================================
 
 ww_iv_reg <- ivreg(fertility$weeksm1 ~ 1 + fertility$morekids | fertility$samesex)
 summary(ww_iv_reg)
 
-
+# The fertility effect on labor supply is small: 0.01431 - 0.01388 = 0.00043
 
 # (f) ==========================================================================
+
+weeksm_lm_2 <- lm(fertility$weeksm1 ~ 1 + fertility$morekids
+                                        + fertility$agem1
+                                        + fertility$black
+                                        + fertility$hispan
+                                        + fertility$othrace)
+summary(weeksm_lm_2)
+
+# Yes, our results change when we add the variables agem1, black, hispan, and 
+# othrace in the labor supply regression (the multiple R-squared is bigger). This 
+# is a result of the additional regressors improving the fit of the model.
+
+
 
